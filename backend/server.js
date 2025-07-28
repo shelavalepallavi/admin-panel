52cbe25b045aacda32494df6075717b39042a550
shelavale3795 <pallavishelavale3795@gmail.com>
2025-07-28 17:35:50 +0530

    update main file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors')
-// require('dotenv').config();
 const connectDB = require('./config/db.js')
 const userRoute = require('./routes/userRoute.js')
 const PORT = 3000
